51458b2139e2e021ddf568d7d8d2c68bd23d0b31
Filipe Ximenes <filipeximenes@gmail.com>
2013-06-16 17:20:41 -0300

    adding allowed hosts

--- ogigante/settings.py
+++ ogigante/settings.py
@@ -20,7 +20,7 @@ DATABASES['default'] =  dj_database_url.config()
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ogiganteacordou.herokuapp.com/']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
