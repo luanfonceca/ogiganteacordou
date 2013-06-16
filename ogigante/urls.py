052af405f9a3b77b4c7933f21082562c1c06c4ac
Filipe Ximenes <filipeximenes@gmail.com>
2013-06-16 17:39:17 -0300

    serving static with django

--- ogigante/urls.py
+++ ogigante/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -6,6 +7,7 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
+    url(r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     url(r'^$', 'core.views.index', name='index'),
     url(r'^eventos$', 'core.views.events', name='events'),
     url(r'^novo$', 'core.views.new_entry', name='new_entry'),
