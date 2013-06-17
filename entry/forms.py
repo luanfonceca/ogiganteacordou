2eb3c13ceed53e7fd7761b0fc5b60cfe4b23311d
Jonatas CD <jonatas.cd@gmail.com>
2013-06-17 14:01:57 -0300

    o "tipo" deve ser um dropdown

--- entry/forms.py
+++ entry/forms.py
@@ -1,8 +1,17 @@
+#encoding: utf-8
 from django.forms import ModelForm
+from django import forms
 from .models import Entry
 
+TIPOS = [
+    ('video', u'Vídeo'),
+    ('link', 'Link'),
+    ('evento', 'Evento')
+]
 
 class NewEntryForm(ModelForm):
     class Meta:
         model = Entry
         exclude = ('approved', )
+
+    kind = forms.ChoiceField(label='Tipo',choices=TIPOS)
