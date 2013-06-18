82aeef2f2fcf83a2da4c2d7b920f702fdf8a2fbe
Patrick Costa <patrickrbcosta@gmail.com>
2013-06-17 22:59:43 -0300

    improving layout

---
+++ ogigante/static/js/script.js
@@ -0,0 +1,19 @@
+var cur = $('.posts div:first-child');
+cur.addClass('focus');
+
+$('html, body').on('keyup', function(e) {
+	e.stopPropagation();
+	var code = (e.keyCode ? e.keyCode : e.which);
+	console.log(code);
+	if (code == 74 || code == 40) {
+		cur.removeClass('focus');
+		if ( cur.next().length > 0 ) cur = cur.next();
+		cur.addClass('focus');
+		$('html, body').scrollTop(cur.offset().top-100);
+	} else if (code == 75 || code == 38 ) {
+		cur.removeClass('focus');
+		if ( cur.prev().length > 0 ) cur = cur.prev();
+		cur.addClass('focus');
+		$('html, body').scrollTop(cur.offset().top-100);
+	}
+});
