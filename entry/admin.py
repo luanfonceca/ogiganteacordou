b5ed73bc2bd54a880daa7af4766eca3deb22299b
Renato Oliveira <renatooliveira.cin@gmail.com>
2013-06-18 13:53:02 -0300

    Changed admin to prioritize unreviewed links.

--- entry/admin.py
+++ entry/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Entry
 
-admin.site.register(Entry)
+
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'text', 'approved', )
+    model = Entry
+    ordering = ['approved']
+
+admin.site.register(Entry, EntryAdmin)
